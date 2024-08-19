2d89425b1b87712546f3ba0ffdf2a7dabfcac81e
rebecagomesferreira <rebeca.gomes@a.ucb.br>
2024-08-19 17:41:31 -0300

    ex02.c

---
+++ ex02.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+	
+	float base, altura, area;
+	
+	printf("Digite a base do retangulo:");
+	scanf("%f", &base);
+
+    printf("Digite a altura do retangulo: ");
+    scanf("%f", &altura);
+
+    area = base * altura;
+
+    printf("A area do retangulo e: %.2f\n", area);
+
+    return 0;
+}
