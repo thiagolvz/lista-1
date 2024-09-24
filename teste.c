08ee0b985998a7feed496ad0f7ce70f18423a57c
rebecagomesferreira <rebeca.gomes@a.ucb.br>
2024-09-24 09:34:08 -0300

    Add files via upload

--- teste.c
+++ teste.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
-#include <locale.h>
 
-//define o número máximo de mensagens que podem ser lidas e o máximo de caracteres por mensagem
+//define o n?mero m?ximo de mensagens que podem ser lidas e o m?ximo de caracteres por mensagem
 #define MAX_MESSAGENS 10000
 #define MAX_CARACTERES 50
 
-//valores da função
+//valores da fun??o
 #define A0 186.752
 #define A1 -148.235
 #define A2 34.5049
@@ -17,32 +16,34 @@
 #define A6 0.0000735464
 #define A7 -4.22038e-7
 
-//calculo da função
-double funcao(int x, int b) {
-    return A0 + (A1 + b) * x 
-           + A2 * pow(x, 2) //pow == função da biblioteca math que eleva x a n 
+//calculo da funçao
+int funcao(int x, int b) {
+	double y = A0 + (A1 + b) * x 
+           + A2 * pow(x, 2) //pow == funçao da biblioteca math que eleva x a n 
            + A3 * pow(x, 3)
            + A4 * pow(x, 4)
            + A5 * pow(x, 5)
            + A6 * pow(x, 6)
            + A7 * pow(x, 7);
+           
+           return round(y); 
 }
 
-//função para decodificar a mensagem
+//funçao para decodificar a mensagem
 //mensagem_hex == array para decodificar o par de digitos da mensagem
 //MAX_CARACTERES * 2 + 1 == 50 * (dois caracteres a serem lidos) + valor de x
 void decodificar_mensagem(char mensagem_hex[MAX_CARACTERES * 2 + 1],  int b) {
-    int i = 0; //posição da string 
-    int x = 1;  //posição atual do caracter
+    int i = 0; //posi??o da string 
+    int x = 1;  //posi??o atual do caracter
     while (mensagem_hex[i] != '\0' && i < MAX_CARACTERES * 2) {
         //leitura dos dois caracteres 
-        //hex_caracteres == array de 3 caracteres que armazena os dois dígitos da mensagem
+        //hex_caracteres == array de 3 caracteres que armazena os dois d?gitos da mensagem
         char hex_caracteres[3];
         hex_caracteres[0] = mensagem_hex[i];
         hex_caracteres[1] = mensagem_hex[i + 1];
         hex_caracteres[2] = '\0';
         
-        //tabela ascii para a conversão 
+        //tabela ascii para a convers?o 
         int ascii = (int)strtol(hex_caracteres, NULL, 16);
         
         //00 == encerra a mensagem
@@ -50,13 +51,15 @@ void decodificar_mensagem(char mensagem_hex[MAX_CARACTERES * 2 + 1],  int b) {
             break;
         }
         
-        //aplicação da função
-        double resultado = funcao(x, b);
+        //aplica??o da fun??o
+        int resultado = funcao(x, b);
         
-        //printa o caractere já decodificado
-        printf("%c", ascii);
+        //printa o caractere j? decodificado
+        if(resultado != 0){
+        	printf("%c", ascii);
+		}
         
-        // incrementa o x e passa para os próximos dois caracteres
+        // incrementa o x e passa para os pr?ximos dois caracteres
         x++;
         i += 2;
     }
@@ -64,17 +67,16 @@ void decodificar_mensagem(char mensagem_hex[MAX_CARACTERES * 2 + 1],  int b) {
 }
 
 int main() {
-	setlocale(LC_ALL, "Portuguese");
     int n, i, b;
     char mensagem_hex[MAX_CARACTERES * 2 + 1];
     
-    //le o número de mensagens a serem decodificadas 
-    printf("Digite o número de mensagens a serem decodificadas (1 a 10000): ");
+    //le o n?mero de mensagens a serem decodificadas 
+    printf("Digite o n?mero de mensagens a serem decodificadas (1 a 10000): ");
     scanf("%d", &n);
     
-    //caso o usuário digite 0 para mensagens a serem decodificadas
+    //caso o usu?rio digite 0 para mensagens a serem decodificadas
     if (n < 1 || n > MAX_MESSAGENS) {
-        printf("Número inválido de mensagens.\n");
+        printf("N?mero inv?lido de mensagens.\n");
         return 1;
     }
     
@@ -87,13 +89,7 @@ int main() {
         //le o valor de b
         printf("Digite o valor de b (-100 a 100): ");
         scanf("%d", &b);
-        
-        //casp b seja menor que -100 ou maior que 100
-        if (b < -100 || b > 100) {
-            printf("Valor de b inválido.\n");
-            return 1;
-        }
-        
+
         //print da mensagem decodificada 
         printf("Mensagem decodificada: ");
         decodificar_mensagem(mensagem_hex, b);
@@ -102,3 +98,7 @@ int main() {
     return 0;
 }
 
+
+//função em arquivo separado  
+//trocar as ordens de leitura, quantas mensagens a serem lidas, b, mensagem
+//tirar os printf e mensagem decodificada
